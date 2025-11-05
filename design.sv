abe7c3e51229a8ba2bca69ca8ceeb530df80e8af
JadonXia1 <jxia073@uottawa.ca>
2025-11-04 19:41:11 -0500

    Delete design.sv

--- design.sv
+++
@@ -1,9 +0,0 @@
-// 32 bit signed integer multiplier
-
-module multiplier (
-  input  wire signed [31:0] a,  
-  input  wire signed [31:0] b, 
-  output wire signed [31:0] c   // 64 bit for overflow--i forgot what we decided upon last meeting for this scenario
-);
-  assign c = a * b;
-endmodule
